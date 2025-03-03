6b5d6652b59bd132353de269fb32f63d7338fef0
VpgMackan <mackan.developer@gmail.com>
2025-03-03 19:42:54 +0100

    Add mock data and store in localStorage on component mount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+import { Sets } from "../../types/types";
+
+const mockdata: Sets[] = [
+  {
+    name: "Engelska Glossor Vecka 14",
+    cards: [
+      {
+        back: "Hallå",
+        front: "Hello",
+        number: 0,
+        id: "253ea86d-a086-4106-af6e-fe57cc889c0a",
+      },
+    ],
+    color: "#2b8d8d",
+    id: "c0914c03-94d9-4bae-ad6a-4cae74a32103",
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+  useEffect(() => {
+    window.localStorage.setItem("sets", JSON.stringify(mockdata));
+  }, []);
+
+  return <div className=""></div>;
 }
